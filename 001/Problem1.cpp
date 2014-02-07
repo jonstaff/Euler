4b8404f047a5605c5a88d57bda8d7513d7184112
jonstaff <jstaff107@gmail.com>
2014-02-07 13:15:52 -0500

    Problem1 solved.

---
+++ 001/Problem1.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char const *argv[]) {
+	int sum = 0;
+	
+	for (int i = 0; i < 999; ++i) {
+		if (i % 3 == 0) {
+			sum += i;
+		} else if (i % 5 == 0) {
+			sum += i;
+		}
+	}
+
+	cout << "The sum is: " << sum << endl;
+
+	return 0;
+}

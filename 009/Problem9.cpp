0d24d82459871520a4ea66c546c556c1dab053cc
jonstaff <jstaff107@gmail.com>
2014-02-11 20:33:25 -0500

    Solution to Problem 9

---
+++ 009/Problem9.cpp
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+using namespace std;
+
+int main(int argc, char const *argv[]) {
+	for (int a = 1; a < 501; ++a) {
+		for (int b = a; b < 501; ++b) {
+			for (int c = b; c < 501; ++c) {
+				if (a * a + b * b == c * c && a + b + c == 1000) {
+					printf("The solution is: %i\n", a * b * c);
+				}
+			}
+		}
+	}
+}

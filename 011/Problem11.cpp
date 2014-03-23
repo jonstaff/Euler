9cdbf05c0c50fad4b4b370f224a2bb77cb93b1d7
jonstaff <jstaff107@gmail.com>
2014-03-23 13:16:00 -0400

    Minor error in the code fixed

--- 011/Problem11.cpp
+++ 011/Problem11.cpp
@@ -36,7 +36,7 @@ int main(int argc, char const *argv[]) {
 
 	for (int i = 3; i < 20; ++i) {
 		for (int j = 3; j < 20; ++j) {
-			temp = grid[i][j] * grid[i-1][j-1] * grid[i-2][j-2] * grid[i-3][j-3];
+			temp = grid[i][j] * grid[i-1][j+1] * grid[i-2][j+2] * grid[i-3][j+3];
 			if (temp > product) product = temp;
 		}
 	}

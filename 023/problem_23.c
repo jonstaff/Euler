3aa1ad9293d929883abbff065fd72751f39ebcea
Jonathon Staff <jon@nplexity.com>
2014-09-23 16:30:54 +0800

    Optimized solution to problem 23

--- 023/problem_23.c
+++ 023/problem_23.c
@@ -2,43 +2,56 @@
 
 const int kMax = 28123;
 
+int is_abundant(int n)
+{
+  int divisors = 0;
+  for (int i = 2; i * i <= n; ++i) {
+    if (!(n % i)) {
+      divisors += i;
+
+      if (i < n / i) {
+        divisors += n / i;
+      }
+
+      if (divisors > n) {
+        return 1;
+      }
+    }
+  }
+
+  return 0;
+}
+
 int main()
 {
-  int abundant_nums[kMax];
+  int abundant[kMax];
+  int checked[kMax];
   int count = 0;
-  for (int i = 12; i < kMax; ++i) {
-    int sum = 0;
 
-    for (int j = 1; j < i; ++j) {
-      if (i % j == 0) {
-        sum += j;
-      }
-    }
-
-    if (sum > i) {
-      abundant_nums[count++] = i;
+  for (int i = 1; i < kMax; ++i) {
+    if (is_abundant(i)) {
+      abundant[count++] = i;
     }
   }
 
-  int is_abundant[kMax];
   for (int i = 0; i < count; ++i) {
-    for (int j = 0; j < count; ++j) {
-      int sum = abundant_nums[i] + abundant_nums[j];
+    for (int j = i; j < count; ++j) {
+      int temp = abundant[i] + abundant[j];
 
-      if (sum < kMax) {
-        is_abundant[sum] = 1;
+      if (temp < kMax) {
+        checked[temp] = 1;
       }
     }
   }
 
-  int total = 0;
+  int sum = 0;
   for (int i = 0; i < kMax; ++i) {
-    if (!is_abundant[i]) {
-      total += i;
+    if (!checked[i]) {
+      sum += i;
     }
   }
 
-  printf("The solution is: %d\n", total);
+  printf("The solution is: %d\n", sum);
 
   return 0;
 }
